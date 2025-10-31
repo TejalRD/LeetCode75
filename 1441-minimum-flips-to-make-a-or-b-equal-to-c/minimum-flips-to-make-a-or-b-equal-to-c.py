6411faed5fd8d4510a9c19362ff1cd988fdbdd12
Tejal Daga <90956881+TejalRD@users.noreply.github.com>
2025-10-31 13:06:29 -0400

    Time: 27 ms (97.58%) | Memory: 17.6 MB (94.61%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -1,28 +1,13 @@
-# class Solution:
-#     def minFlips(self, a: int, b: int, c: int) -> int:
-#         flips = 0
-#         while a or b or c:
-#             a_bit, b_bit, c_bit= a&1, b&1, c&1
-#             if (a_bit | b_bit) !=c_bit:
-#                 if c_bit ==1:
-#                     flips +=1
-#                 else:
-#                     flips += (b_bit + b_bit)
-#             a >>= 1
-#             b >>= 1
-#             c >>= 1
-#         return flips
-
 class Solution:
     def minFlips(self, a: int, b: int, c: int) -> int:
         flips = 0
-        while a or b or c:  # <-- include c here
-            a_bit, b_bit, c_bit = a & 1, b & 1, c & 1
-            if (a_bit | b_bit) != c_bit:
-                if c_bit == 1:
-                    flips += 1
+        while a or b or c:
+            a_bit, b_bit, c_bit= a&1, b&1, c&1
+            if (a_bit | b_bit) !=c_bit:
+                if c_bit ==1:
+                    flips +=1
                 else:
-                    flips += (a_bit + b_bit)  # 1 flip if one is 1, 2 flips if both are 1
+                    flips += (a_bit + b_bit)
             a >>= 1
             b >>= 1
             c >>= 1
